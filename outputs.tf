7ab0c315b7ad924b746f8002bbc1507886677c23
George Fortetsanakis <gfortetsanakis@gmail.com>
2022-09-02 15:50:07 +0300

    Converted subnets_ids to a list

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "eks_cluster_properties" {
     eks_cluster_ca_cert         = aws_eks_cluster.test_cluster.certificate_authority[0].data
     openid_connect_provider_arn = aws_iam_openid_connect_provider.eks_cluster_oidc_provider.arn
     openid_connect_provider_url = aws_iam_openid_connect_provider.eks_cluster_oidc_provider.url
-    subnet_ids                  = aws_eks_cluster.test_cluster.vpc_config[0].subnet_ids
+    subnet_ids                  = tolist(aws_eks_cluster.test_cluster.vpc_config[0].subnet_ids)
     eks_cluster_sg_id           = aws_eks_cluster.test_cluster.vpc_config[0].cluster_security_group_id
     vpc_id                      = aws_eks_cluster.test_cluster.vpc_config[0].vpc_id
   }
